1c684fc3d77311a5f8e5cf734fbf2c4267836321
谢俊 <xiejun_ncu@qq.com>
2015-07-16 20:59:02 +0800

    微信接口开发接收微信消息
    C#微信公众号接口开发之从零开发（二） 接收微信消息并且解析XML

---
+++ sohovan.com.wxweb/sohovan.com.wxapi/WxXmlModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sohovan.com.wxapi
+{
+    /// <summary>
+    /// 微信接口XmlModel
+    /// XML解析
+    /// </summary>
+    public class WxXmlModel
+    {
+        /// <summary>
+        /// 消息接收方微信号
+        /// </summary>
+        public string ToUserName { get; set; }
+    
+        /// <summary>
+        /// 消息发送方微信号
+        /// </summary>
+        public string FromUserName { get; set; }
+       
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public string CreateTime { get; set; }
+   
+        /// <summary>
+        /// 信息类型 地理位置:location,文本消息:text,消息类型:image
+        /// </summary>
+        public string MsgType { get; set; }
+      
+        /// <summary>
+        /// 信息内容
+        /// </summary>
+        public string Content { get; set; }
+       
+        /// <summary>
+        /// 地理位置纬度
+        /// </summary>
+        public string Location_X { get; set; }
+   
+        /// <summary>
+        /// 地理位置经度
+        /// </summary>
+        public string Location_Y { get; set; }
+        /// <summary>
+        /// 地图缩放大小
+        /// </summary>
+        public string Scale { get; set; }
+        /// <summary>
+        /// 地理位置信息
+        /// </summary>
+        public string Label { get; set; }
+        /// <summary>
+        /// 图片链接，开发者可以用HTTP GET获取
+        /// </summary>
+        public string PicUrl { get; set; }
+        
+        /// <summary>
+        /// 事件类型，subscribe(订阅/扫描带参数二维码订阅)、unsubscribe(取消订阅)、CLICK(自定义菜单点击事件) 、SCAN（已关注的状态下扫描带参数二维码）
+        /// </summary>
+        public string Event { get; set; }
+
+        /// <summary>
+        /// 事件KEY值
+        /// </summary>
+        public string EventKey { get; set; }
+
+        /// <summary>
+        /// 二维码的ticket，可以用来换取二维码
+        /// </summary>
+        public string Ticket { get; set; }
+    }
+}
--- sohovan.com.wxweb/sohovan.com.wxdemo/API.ashx.cs
+++ sohovan.com.wxweb/sohovan.com.wxdemo/API.ashx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.SessionState;
 using System.Web.Security;
+using sohovan.com.wxapi;
+using System.IO;
+using System.Text;
+using System.Xml;
 
 namespace sohovan.com.wxdemo
 {
@@ -23,6 +27,7 @@ namespace sohovan.com.wxdemo
                 if (CheckSignature())
                 {
                     //接收消息
+                    ReceiveXml();
                 }
                 else
                 {
@@ -83,6 +88,67 @@ namespace sohovan.com.wxdemo
                 return false;
             }
         }
+        #endregion
+
+        #region 接收消息
+/// <summary>
+/// 接收微信发送的XML消息并且解析
+/// </summary>
+private void ReceiveXml()
+{
+    Stream requestStream = System.Web.HttpContext.Current.Request.InputStream;
+    byte[] requestByte = new byte[requestStream.Length];
+    requestStream.Read(requestByte, 0, (int)requestStream.Length);
+    string requestStr = Encoding.UTF8.GetString(requestByte);
+
+    if (!string.IsNullOrEmpty(requestStr))
+    {
+        //封装请求类
+        XmlDocument requestDocXml = new XmlDocument();
+        requestDocXml.LoadXml(requestStr);
+        XmlElement rootElement = requestDocXml.DocumentElement;
+
+        WxXmlModel WxXmlModel = new WxXmlModel();
+        WxXmlModel.ToUserName = rootElement.SelectSingleNode("ToUserName").InnerText;
+        WxXmlModel.FromUserName = rootElement.SelectSingleNode("FromUserName").InnerText;
+        WxXmlModel.CreateTime = rootElement.SelectSingleNode("CreateTime").InnerText;
+        WxXmlModel.MsgType = rootElement.SelectSingleNode("MsgType").InnerText;
+
+        switch (WxXmlModel.MsgType)
+        {
+            case "text":
+                WxXmlModel.Content = rootElement.SelectSingleNode("Content").InnerText;
+                break;
+            case "image":
+                WxXmlModel.PicUrl = rootElement.SelectSingleNode("PicUrl").InnerText;
+                break;
+            case "event":
+                WxXmlModel.Event = rootElement.SelectSingleNode("Event").InnerText;
+                if (WxXmlModel.Event == "subscribe")//关注类型
+                {
+                    WxXmlModel.EventKey = rootElement.SelectSingleNode("EventKey").InnerText;
+                }
+                break;
+            default:
+                break;
+        }
+        sohovan.com.common.CommonMethod.WriteTxt(WxXmlModel.Content);//接收的文本消息
+        //回复消息<xml>
+        //<ToUserName><![CDATA[toUser]]></ToUserName>
+        //<FromUserName><![CDATA[fromUser]]></FromUserName>
+        //<CreateTime>12345678</CreateTime>
+        //<MsgType><![CDATA[text]]></MsgType>
+        //<Content><![CDATA[你好]]></Content>
+        //<FuncFlag>0</FuncFlag>
+        //</xml>
+        string XML = "<xml><ToUserName><![CDATA[" + WxXmlModel.FromUserName + "]]></ToUserName><FromUserName><![CDATA[" + WxXmlModel.ToUserName + "]]></FromUserName>";
+        XML +="<CreateTime>" + sohovan.com.common.CommonMethod.ConvertDateTimeInt(DateTime.Now) + "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[接收消息成功,消息为" + WxXmlModel.Content + "]]></Content><FuncFlag>0</FuncFlag></xml>";
+        //ResponseXML(WxXmlModel);
+        sohovan.com.common.CommonMethod.WriteTxt(XML);
+        HttpContext.Current.Response.Write(XML);
+        HttpContext.Current.Response.End();
+    }
+}
         #endregion
 
         public bool IsReusable
